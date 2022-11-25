ac140737ae7cff28b0b3ed9934ad17a1f7285e60
ebrahimkamari <ebrahim.kamari@gmail.com>
2022-11-25 14:06:29 -0800

    update

--- eks/eks-tools-setup.sh
+++ eks/eks-tools-setup.sh
@@ -16,7 +16,7 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3.us-east-1.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 kubectl version --short --client
